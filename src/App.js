8b8c9e734a414b959a5c5b09680ecf74d85f2970
Deraclassic <chiderauzoigwe2@gmail.com>
2024-04-24 19:42:20 +0100

    commit react algorithm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class UniqueList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      fruits:"",
+      fruitList:[]
+    }
+    
+    this.handleChange=this.handleChange.bind(this)
+  }
+  
+  handleChange(e){
+    this.setState({fruits:e.target.value.trim()})
+  }
+  
+  addItems(){
+      const isExist = this.state.fruitList.find(item => item == this.state.fruits.trim())
+      
+      if(!isExist){
+         this.setState({fruits:"", 
+                   fruitList:[...this.state.fruitList, this.state.fruits.trim()]})
+      } 
+  }
+  
+  removeItem(fruitName){
+    const remainingFruits = this.state.fruitList.filter(item => item !== fruitName)
+    this.setState({fruits:"", fruitList:remainingFruits})
+  }
+  
+  clear(){
+    
+    if(this.state.fruitList.length > 0){
+         this.setState({fruits:"", fruitList:[]})
+    }
+ 
+  }
+  
+  render() {
+    return (
+      <>
+        <div>
+          <input
+            className="item-input"
+            type="text"
+            value={this.state.fruits}
+            onChange={(e) => this.handleChange(e)}
+            onKeyDown={() => this.addItems()}
+          />
+          <input
+            className="add-button"
+            type="button"
+            value="Add Item"
+            onClick={() => this.addItems()}
+          />
+          <input
+            className="remove-button"
+            type="button"
+            value="Remove Item"
+            onClick={() => this.removeItem(this.state.fruits)}
+          />
+          <input
+            className="clear-button"
+            type="button"
+            value="Clear Items"
+            onClick={() => this.clear()}
+          />
+        </div>
+        <ul className="items">
+          {this.state.fruitList.map(item => <li>{item}</li>)}
+        </ul>
+      </>
+    );
+  }
 }
 
-export default App;
+export default UniqueList;
